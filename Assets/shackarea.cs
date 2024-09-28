48353ad12782a4f13ca3db823a1f6f2e406bcfaa
shahar hodera <shaharhodera@gmail.com>
2024-09-28 17:44:49 +0300

    update anim

---
+++ Assets/shackarea.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shackarea : MonoBehaviour
+{
+    StarterAssets.ThirdPersonController tpc;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerArmature");
+        tpc = player.GetComponent<StarterAssets.ThirdPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	private void OnTriggerStay(Collider other)
+	{
+        if (other.CompareTag("Player"))
+        {
+            tpc.PlayerState = 4;
+            tpc.cold = 5;
+            tpc.reduse = false;
+        }
+    }
+	private void OnTriggerExit(Collider other)
+	{
+        if (other.CompareTag("Player"))
+        {
+            tpc.reduse = true;
+        }
+    }
+
+}
